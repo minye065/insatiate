e073e8ad48ea00253e06ed07574cbe19a5cb9d93
minye065 <minye065@fusdk12.net>
2026-09-15 14:56:15 -0700

    init

---
+++ include/enemies.ts
@@ -0,0 +1,12 @@
+// for each enemy
+//{{name}, {stats}, {attacks}, {assets}}
+// {name} = "name"
+// {stats} = {baseHeath, baseSpeed}
+// {attacks} = {attack, attack, attack}; attack = {attackName, attackDamage}
+// {assets} = {asset, asset, asset}; asset = {"name", location}
+
+export const enemies = 
+{
+
+
+}
