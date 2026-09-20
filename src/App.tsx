e485ce76f0dd8d0f1c3e90fe96c060a67cb0931d
felix kuo <felixtheyeet@gmail.com>
2026-09-19 22:54:09 -0700

    funni launch page somewhat

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-export default function App()
-{
-	return (
-		<div>
-			<h1>Hello, world!</h1>
-		</div>
-	);
+import "./styles.css";
+
+function App(){
+    const handleplay = () => {
+        alert("hihithere2");
+    };
+
+    return(
+        /// note: add some more stuff here later like class system or smth idk
+        <main id="home" className="placeholder">
+            <div className="placeholder-content">
+                <h2>splash text</h2>
+                <p>placeholder2</p>
+
+                <button onClick={handleplay}>
+                    plz play we need this speed
+                </button>
+            </div>
+
+        </main>
+
+    );
 }
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-if (!container) throw new Error("root element not found");
-
-createRoot(container).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
